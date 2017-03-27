700ab466ff29b45a6d9f16b2d712364b0c3d8bf1
dgwp-projects <zayneabraham1@gmail.com>
2017-03-27 16:55:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const app = "I don't do much.";
+
+
+const token = 'df12ce078e6347f7d44fbd0837d5cb0cc39d2e57'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
